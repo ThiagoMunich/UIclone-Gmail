b9ad934ccf0e15272a040b57b39c9649702a1822
thiagomunich <thiagomunich@gmail.com>
2021-04-09 16:43:10 -0300

    Trying

--- src/pages/InvalidScreenSize/index.js
+++ src/pages/InvalidScreenSize/index.js
@@ -1,22 +1,15 @@
-import React from 'react';
+import React from 'react'
 
-import { Typography, Grid } from '@material-ui/core';
+import { Typography, Grid } from '@material-ui/core'
 
 function InvalidScreenSize() {
-	return (
-		<Grid
-			container
-			direction='column'
-			alignItems='center'
-			style={{ position: 'fixed', top: '50%', backgroundColor: '#F3F3F3' }}
-		>
-			<Typography variant='h2'>Invalid screen size</Typography>
-			<Typography variant='body1'>
-				Please orient your device to portrait or find a smaller screen. You won't be disappointed.
-			</Typography>
-			<Typography variant='caption'>Max width allowed: 1000px</Typography>
-		</Grid>
-	);
+  return (
+    <Grid container direction="column" alignItems="center" style={{ position: 'fixed', top: '50%', backgroundColor: '#F3F3F3' }}>
+      <Typography variant="h2">Invalid screen size!</Typography>
+      <Typography variant="body1">Please orient your device to portrait or find a smaller screen. You won't be disappointed.</Typography>
+      <Typography variant="caption">Max width allowed: 1000px</Typography>
+    </Grid>
+  )
 }
 
-export default InvalidScreenSize;
+export default InvalidScreenSize
