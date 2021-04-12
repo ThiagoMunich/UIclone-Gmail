2f8370bd2034778d5d9b6efc8eeb7bbf7d328680
thiagomunich <thhiagomunich@gmail.com>
2021-04-12 19:55:56 -0300

    Minha stack

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,48 +1,48 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Avatar from '@material-ui/core/Avatar';
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import InputBase from '@material-ui/core/InputBase'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import Avatar from '@material-ui/core/Avatar'
 
-import profilePicture from '../../assets/images/profilePicture.jpg';
-import DialogNotImplemented from '../DialogNotImplemented';
+import profilePicture from '../../assets/images/profilePicture.jpg'
+import DialogNotImplemented from '../DialogNotImplemented'
 
-const useStyles = makeStyles(theme => ({
-	root: {
-		display: 'flex',
-		alignItems: 'center',
-		width: '100%',
-		height: 50,
-		marginBottom: 10,
-	},
-	input: {
-		marginLeft: theme.spacing(1),
-		flex: 1,
-	},
-	iconButton: {
-		padding: 10,
-	},
-}));
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    width: '100%',
+    height: 50,
+    marginBottom: 10,
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+}))
 
 export default function Header() {
-	const classes = useStyles();
-	const [openDialog, setOpenDialog] = useState(false);
+  const classes = useStyles()
+  const [openDialog, setOpenDialog] = useState(false)
 
-	return (
-		<>
-			<Paper component='form' className={classes.root}>
-				<IconButton className={classes.iconButton} onClick={() => setOpenDialog(true)}>
-					<MenuIcon />
-				</IconButton>
-				<InputBase className={classes.input} placeholder='Search e-mail' />
+  return (
+    <>
+      <Paper component="form" className={classes.root}>
+        <IconButton className={classes.iconButton} onClick={() => setOpenDialog(true)}>
+          <MenuIcon />
+        </IconButton>
+        <InputBase className={classes.input} placeholder="Search e-mail" />
 
-				<IconButton className={classes.iconButton}>
-					<Avatar alt='kabal' src={profilePicture} />
-				</IconButton>
-			</Paper>
-			<DialogNotImplemented open={openDialog} close={() => setOpenDialog(false)} />
-		</>
-	);
+        <IconButton className={classes.iconButton}>
+          <Avatar alt="kabal" src={profilePicture} />
+        </IconButton>
+      </Paper>
+      <DialogNotImplemented open={openDialog} close={() => setOpenDialog(false)} />
+    </>
+  )
 }
--- src/pages/OpenMail/index.js
+++ src/pages/OpenMail/index.js
@@ -2,14 +2,22 @@ import React from 'react'
 
 import { Dialog, DialogContent, Grid, Slide, Avatar, Typography } from '@material-ui/core'
 
+import Alert from '@material-ui/lab/Alert'
+
 import { makeStyles } from '@material-ui/core/styles'
 
+import { IoLogoPwa } from 'react-icons/io5'
+import { SiMicrosoftsqlserver } from 'react-icons/si'
+import { DiDigitalOcean } from 'react-icons/di'
 import ArrowBackIcon from '@material-ui/icons/ArrowBack'
 import UndoOutlinedIcon from '@material-ui/icons/UndoOutlined'
+import { FaReact, FaLaravel, FaDigitalOcean } from 'react-icons/fa'
 import MoreVertOutlinedIcon from '@material-ui/icons/MoreVertOutlined'
 import MailOutlineOutlinedIcon from '@material-ui/icons/MailOutlineOutlined'
 import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined'
 import SystemUpdateAltOutlinedIcon from '@material-ui/icons/SystemUpdateAltOutlined'
+
+import profilePicture from '../../assets/images/profilePicture.jpg'
 
 const useStyles = makeStyles({
   backIcon: {
@@ -51,7 +59,7 @@ function OpenMail({ open, close, mail }) {
         <Grid container style={{ padding: 8 }} justify="space-between" alignItems="center">
           <Avatar style={{ backgroundColor: mail.avatarColor }}>{mail.avatarLetter}</Avatar>
           <Typography variant="body2" className={classes.title}>
-            This text just random
+            I'm glad you're here!
           </Typography>
 
           <Grid item container md={5} sm={5} xs={5} justify="flex-end">
@@ -60,7 +68,71 @@ function OpenMail({ open, close, mail }) {
           </Grid>
         </Grid>
 
-        <DialogContent>{mail.content}</DialogContent>
+        <Grid container style={{ padding: 10 }}>
+          <Grid item md={12} sm={12} xs={12}>
+            <Typography variant="h5" component="p">
+              Hello there!
+            </Typography>
+          </Grid>
+          <Grid item md={12} sm={12} xs={12}>
+            <Typography variant="body2" component="p">
+              My name is Thiago Munich and I'm a full stack developer.
+            </Typography>
+          </Grid>
+        </Grid>
+
+        <Grid container justify="center">
+          <img src={profilePicture} alt="kabal" style={{ borderRadius: 360, width: '50%' }} />
+        </Grid>
+
+        <Grid container style={{ padding: 10, marginTop: 15 }} justify="space-between">
+          <Grid item>
+            <Typography variant="caption">
+              <strong>Experience:</strong> nearly 2 years
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="caption">
+              <strong>Availability:</strong> preferably freelances
+            </Typography>
+          </Grid>
+        </Grid>
+
+        <Typography variant="overline" style={{ fontWeight: 700, textAlign: 'center' }}>
+          Technologies
+        </Typography>
+
+        <Grid container direction="row" alignItems="center" justify="space-between" style={{ padding: 10 }} spacing={2}>
+          <Grid container item alignItems="center" justify="space-between" md={5} sm={5} xs={5}>
+            <FaReact size={20} style={{ color: '#61DBFB' }} />
+            <Typography variant="caption">React JS</Typography>
+          </Grid>
+
+          <Grid container item alignItems="center" justify="space-between" md={5} sm={5} xs={5}>
+            <FaReact size={20} style={{ color: '#61DBFB' }} />
+            <Typography variant="caption">React Native</Typography>
+          </Grid>
+
+          <Grid container item alignItems="center" justify="space-between" md={5} sm={5} xs={5}>
+            <IoLogoPwa size={20} style={{ color: '#1d395d' }} />
+            <Typography variant="caption">React PWA</Typography>
+          </Grid>
+
+          <Grid container item alignItems="center" justify="space-between" md={5} sm={5} xs={5}>
+            <FaLaravel size={20} style={{ color: '#F05340' }} />
+            <Typography variant="caption">Laravel</Typography>
+          </Grid>
+
+          <Grid container item alignItems="center" justify="space-between" md={5} sm={5} xs={5}>
+            <SiMicrosoftsqlserver size={20} style={{ color: '#F25022' }} />
+            <Typography variant="caption">SQL Server</Typography>
+          </Grid>
+
+          <Grid container item alignItems="center" justify="space-between" md={5} sm={5} xs={5}>
+            <FaDigitalOcean size={20} style={{ color: '#008bcf' }} />
+            <Typography variant="caption">Cloud Computing</Typography>
+          </Grid>
+        </Grid>
       </Dialog>
     </>
   )
